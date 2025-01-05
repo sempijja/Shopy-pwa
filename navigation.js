3524738011dfb3033564976a8338165707f58473
Ssempijja Charles Charz <ssempijacharz@gmail.com>
2025-01-05 19:33:21 +0300

    Enhance onboarding experience by adding WhatsApp verification screen and improving button functionality; update styles for better layout

--- navigation.js
+++ navigation.js
@@ -1,7 +1,13 @@
-document.getElementById('google-button').addEventListener('click', function() {
-    window.location.href = 'google-auth.html'; // Replace with your target HTML file
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('google-button').addEventListener('click', function() {
+      window.location.href = 'google-auth.html'; // target HTML file
   });
 
   document.getElementById('whatsapp-button').addEventListener('click', function() {
-    window.location.href = 'screens/add-number-whatsapp.html'; // Replace with your target HTML file
+      window.location.href = 'screens/add-number-whatsapp.html'; // target HTML file
   });
+
+  document.getElementById('verify-number').addEventListener('click', function() {
+      window.location.href = 'screens/number-whatsapp-verify.html'; // target HTML file
+  });
+});
