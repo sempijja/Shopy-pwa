0e2f083c6afc7c2dad420786bb1e4781bf7dd40a
Ssempijja Charles Charz <ssempijacharz@gmail.com>
2025-01-06 15:50:05 +0300

    Add store onboarding page and update button functionality; modify manifest and styles for improved layout

--- navigation.js
+++ navigation.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('google-button').addEventListener('click', function() {
-      window.location.href = 'google-auth.html'; // target HTML file
-  });
+  const setupStoreButton = document.getElementById('setup-store-button');
+  if (setupStoreButton) {
+    setupStoreButton.addEventListener('click', function() {
+      window.location.href = 'store-onboard-about-business.html'; // target HTML file
+    });
+  } else {
+    console.error('Element with ID "setup-store-button" not found.');
+  }
 
-  document.getElementById('whatsapp-button').addEventListener('click', function() {
+  const whatsappButton = document.getElementById('whatsapp-button');
+  if (whatsappButton) {
+    whatsappButton.addEventListener('click', function() {
       window.location.href = 'screens/add-number-whatsapp.html'; // target HTML file
-  });
+    });
+  } else {
+    console.error('Element with ID "whatsapp-button" not found.');
+  }
 });
