7c53f8b7bd1b6f75a0d44fe80a3b62c84031f669
Ssempijja Charles Charz <ssempijacharz@gmail.com>
2025-01-14 22:56:56 +0300

    Fix navigation path for onboarding to point to the correct HTML file

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .finally(() => {
             // Navigate to the next screen regardless of the fetch result
-            window.location.href = '/screens/store-onboard-add-physical-products.html';
+            window.location.href = '../screens/store-onboard-add-physical-products.html';
         });
     });
 });
